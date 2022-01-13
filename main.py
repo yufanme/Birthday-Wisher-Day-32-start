a25cc2147c865af628367a96789ed30a3a49f45f
Fan Yu <562937707@qq.com>
2022-01-13 17:48:53 +0800

    can send email now.

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
     connection.sendmail(from_addr=my_email,
-                        to_addrs="562937707.com",
+                        to_addrs="562937707@qq.com",
                         msg="Subject:Hello\n\nThis is the body of my email.")
 
